1d77336fdd500ae3cc2d5013d15019c424932882
Ryan Nourbaran <arya.nourbaran@hotmail.com>
2018-10-25 21:10:27 -0400

    eyeball and form in place, added eyeball movement for username in forward direction

--- CoolLogin/index.js
+++ CoolLogin/index.js
@@ -1,17 +1,96 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, Animated, Easing } from "react-native";
+import { Container, Header, Content, Form, Item, Input } from "native-base";
 
+const styles = StyleSheet.create({
+  eyeball: {
+    width: 100,
+    height: 75,
+    borderWidth: 1,
+    borderRadius: 50
+  }
+});
 export default class CoolLogin extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      eyeBallLeftMargin: 0
+    };
+    this.spinValue = new Animated.Value(0);
   }
 
+  userChange() {
+    //add a way to make the aniation in sequence not parralel
+    let oldVal = this.state.eyeBallLeftMargin;
+    console.log(oldVal);
+
+    if (oldVal < 1) {
+      Animated.timing(this.spinValue, {
+        toValue: 0.03 + oldVal,
+        duration: 400,
+        easing: Easing.linear
+      }).start(
+        this.setState({
+          eyeBallLeftMargin: oldVal + 0.03
+        })
+      );
+    }
+  }
+  userBackSpace() {
+    //add a way to make the aniation in sequence not parralel
+    let oldVal = this.state.eyeBallLeftMargin;
+    console.log(oldVal);
+
+    if (oldVal < 1) {
+      Animated.timing(this.spinValue, {
+        toValue: 0.03 + oldVal,
+        duration: 400,
+        easing: Easing.linear
+      }).start(
+        this.setState({
+          eyeBallLeftMargin: oldVal + 0.03
+        })
+      );
+    }
+  }
+  passChange() {
+    console.log("password changed");
+  }
   render() {
+    let { eyeBallLeftPosition } = this.state;
+    const left = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 300]
+    });
+
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <Container>
+        <Header />
+        <Content>
+          <View style={{ flexDirection: "row" }}>
+            <View style={styles.eyeball}>
+              <Animated.Image
+                style={{ width: 50, height: 50, top: 5, left }}
+                source={require("../../assets/inner-eye2.png")}
+              />
+            </View>
+            <View style={styles.eyeball}>
+              <Animated.Image
+                style={{ width: 50, height: 50, top: 5, left }}
+                source={require("../../assets/inner-eye2.png")}
+              />
+            </View>
+          </View>
+          <Form>
+            <Item>
+              <Input placeholder="Username" onChangeText={this.userChange.bind(this)} />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" onChangeText={this.passChange.bind(this)} />
+            </Item>
+          </Form>
+        </Content>
+      </Container>
     );
   }
 }
