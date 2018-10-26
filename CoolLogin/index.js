a253a43d3cdfe6fac13687dc94892c8d17451f54
Ryan Nourbaran <arya.nourbaran@hotmail.com>
2018-10-25 21:12:32 -0400

    added password functionality, limits, and rotation animation

--- CoolLogin/index.js
+++ CoolLogin/index.js
@@ -15,15 +15,16 @@ export default class CoolLogin extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      eyeBallLeftMargin: 0
+      userEyeBallLeftStyle: 0,
+      passEyeBallLeftStyle: 0
     };
-    this.spinValue = new Animated.Value(0);
+    this.left = new Animated.Value(0);
+    this.spin = new Animated.Value(0);
   }
 
   userKeyPress = function(e) {
     //add a way to make the aniation in sequence not parralel
-    let oldVal = this.state.eyeBallLeftMargin;
-    console.log(oldVal);
+    let oldVal = this.state.userEyeBallLeftStyle;
     let changeVal = 0.01;
     const maxVal = 0.32;
     const minVal = 0;
@@ -33,28 +34,73 @@ export default class CoolLogin extends Component {
     }
 
     //set the new val even if it's reached the end
-    const newVal = oldVal + changeVal;
+    if ((newVal = oldVal + changeVal) < 0) {
+      newVal = 0;
+    }
+
     this.setState({
-      eyeBallLeftMargin: newVal
+      userEyeBallLeftStyle: newVal
     });
 
-    //either (old value is below max and increasing) OR (old value above min and decreasing)
+    //Value must be between the min and max. Until then, do no animations (but inc/dec. the value)
     if (oldVal < maxVal && oldVal >= minVal) {
-      Animated.timing(this.spinValue, {
+      Animated.timing(this.left, {
         toValue: newVal,
+        duration: 400,
+        easing: Easing.linear
+      }).start();
+      Animated.timing(this.spin, {
+        toValue: -newVal,
         duration: 400,
         easing: Easing.linear
       }).start();
     }
   };
-  passChange(e) {
-    console.log(e);
-  }
+  passKeyPress = function(e) {
+    //add a way to make the aniation in sequence not parralel
+    let oldVal = this.state.passEyeBallLeftStyle;
+    let changeVal = 0.01;
+    const maxVal = 0.32;
+    const minVal = 0;
+
+    if (e.nativeEvent.key == "Backspace") {
+      changeVal *= -1;
+    }
+
+    //set the new val even if it's reached the end
+    if ((newVal = oldVal + changeVal) < 0) {
+      newVal = 0;
+    }
+
+    this.setState({
+      passEyeBallLeftStyle: newVal
+    });
+
+    //Value must be between the min and max. Until then, do no animations (but inc/dec. the value)
+    if (oldVal < maxVal && oldVal >= minVal) {
+      Animated.timing(this.left, {
+        toValue: newVal,
+        duration: 400,
+        easing: Easing.linear
+      }).start();
+
+      Animated.timing(this.spin, {
+        toValue: -newVal,
+        duration: 400,
+        easing: Easing.linear
+      }).start();
+    }
+  };
+
   render() {
-    let { eyeBallLeftPosition } = this.state;
-    const left = this.spinValue.interpolate({
+    const left = this.left.interpolate({
       inputRange: [0, 1],
       outputRange: [0, 300]
+    });
+
+    const spin = this.spin.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["0deg", "360deg"]
     });
 
     return (
@@ -64,13 +110,13 @@ export default class CoolLogin extends Component {
           <View style={{ flexDirection: "row" }}>
             <View style={styles.eyeball}>
               <Animated.Image
-                style={{ width: 50, height: 50, top: 5, left }}
+                style={{ width: 50, height: 50, top: 15, left, transform: [{ rotate: spin }] }}
                 source={require("../../assets/inner-eye2.png")}
               />
             </View>
             <View style={styles.eyeball}>
               <Animated.Image
-                style={{ width: 50, height: 50, top: 5, left }}
+                style={{ width: 50, height: 50, top: 15, left, transform: [{ rotate: spin }] }}
                 source={require("../../assets/inner-eye2.png")}
               />
             </View>
@@ -84,7 +130,13 @@ export default class CoolLogin extends Component {
               />
             </Item>
             <Item last>
-              <Input placeholder="Password" onChangeText={this.passChange.bind(this)} />
+              <Input
+                placeholder="Password"
+                keyboardType="default"
+                onKeyPress={this.passKeyPress.bind(this)}
+                secureTextEntry={true}
+                onFocus={() => this.setState({ passEyeBallLeftStyle: 0 })}
+              />
             </Item>
           </Form>
         </Content>
